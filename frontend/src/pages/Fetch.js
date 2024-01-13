88f165fe5c6e64877812d5b1d299f0923340b4aa
Shreyash Chatterjee <shreyashchatterjee@gmail.com>
2024-01-14 01:17:30 +0530

    Access to java backend established

--- frontend/src/pages/Fetch.js
+++ frontend/src/pages/Fetch.js
@@ -1,4 +1,33 @@
+import api from "../api/axiosConfig";
+import { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 function Fetch() {
+    const [list, setList] = useState();
+    const location = useLocation();
+    const navigate = useNavigate();
+    const typeList = ["Anime", "Manga"];
+    const watchStatusList = ["Completed", "Current", "Dropped", "Paused", "Planned"];
+    const params = location.pathname.split("/");
+    const [username, setUsername] = useState(params[2]);
+    const [type, setType] = useState(params[3]);
+    const [watchStatus, setWatchStatus] = useState(params[4]);
+
+
+    const getList = async() => {
+        try{
+            const response = await api.get("" + username + "/" + type + "/" + watchStatus);
+            // const response = await api.get("Uchiha268/ANIME/COMPLETED");
+            console.log("Response from server", response);
+            setList(response.data);
+        }
+        catch(error){
+            console.log(error);
+        }  
+    }
+
+    useEffect(() => {  
+        getList();
+    }, [])
     return(
         <>
             <div>
