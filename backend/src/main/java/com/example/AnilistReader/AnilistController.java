363986b7f997e21bd31a1a23dfc79bd00610e528
Shreyash Chatterjee <shreyashchatterjee@gmail.com>
2024-01-14 12:05:48 +0530

    Added CORS annotation in the AnilistController

--- backend/src/main/java/com/example/AnilistReader/AnilistController.java
+++ backend/src/main/java/com/example/AnilistReader/AnilistController.java
@@ -2,12 +2,13 @@ package com.example.AnilistReader;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-
+@CrossOrigin(maxAge = 3600)
 @RestController("api/v1/")
 public class AnilistController {
     @Autowired
